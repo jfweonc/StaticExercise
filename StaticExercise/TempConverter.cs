be32dddee82fbc24172f312830f32464745cf8b7
jfweonc <112230052+jfweonc@users.noreply.github.com>
2022-09-14 00:26:53 -0500

    Update TempConverter.cs

--- StaticExercise/TempConverter.cs
+++ StaticExercise/TempConverter.cs
@@ -1,18 +1,22 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace StaticExercise
 {
-    public static class TempConverter
+    internal class TempConverter
     {
-        public static double FahrenheitToCelsius(double fahrenheit)
+        public double FahrenheitToCelsius(double f)
         {
-            var result = (fahrenheit - 32) / 1.8;
-
-            return result;
+            return (f - 32) * 5 / 9;
         }
 
-        public static double CelsiusToFahrenheit(double celsius)
+
+        public double CelsiusToFahrenheit(double c)
         {
-            return (celsius * 9) / 5 + 32;
+            return (c * 9 / 5) + 32;
         }
     }
 }
